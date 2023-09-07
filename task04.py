c6c231fec9831f4944f944e053a6c74884c29290
jgohl-dci <johannes.gohl@dci-student.org>
2023-09-07 15:56:03 +0200

    ed 4

--- task04.py
+++ task04.py
@@ -1,2 +1,8 @@
 import re
 
+sample_text = 'The quick brown fox jumps over the lazy dog.'
+searched_words = 'fox'
+
+position = re.search('fox', sample_text)
+
+print(f"{position.group()} Start: {position.start()} End: {position.end()}")
